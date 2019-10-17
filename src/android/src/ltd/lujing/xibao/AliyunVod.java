9891f323f2437bd4859cbe1445faca8c263d14c9
maireo <sawako.saki@gmail.com>
2019-10-17 12:25:31 +0800

    修复异步问题

--- src/android/src/ltd/lujing/xibao/AliyunVod.java
+++ src/android/src/ltd/lujing/xibao/AliyunVod.java
@@ -5,9 +5,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Build;
-import android.support.v4.app.ActivityCompat;
 import android.util.Log;
-import android.widget.Toast;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -19,7 +17,6 @@ import org.json.JSONObject;
 import com.alibaba.sdk.android.vod.upload.VODUploadCallback;
 import com.alibaba.sdk.android.vod.upload.VODUploadClient;
 import com.alibaba.sdk.android.vod.upload.VODUploadClientImpl;
-import com.alibaba.sdk.android.vod.upload.common.UploadStateType;
 import com.alibaba.sdk.android.vod.upload.model.UploadFileInfo;
 import com.alibaba.sdk.android.vod.upload.model.VodInfo;
 
@@ -71,115 +68,116 @@ public class AliyunVod extends CordovaPlugin {
                     Context context = this.cordova.getContext();
                     this.startUpload(fileList, callbackContext, context);
                 }
-            });
 
+                private void startUpload(List<VodUploadFileModel> fileList, CallbackContext callbackContext,
+                        Context context) {
+                    final VODUploadClient uploader = new VODUploadClientImpl(context);
+                    uploader.setRegion(VOD_REGION);
+                    uploader.setRecordUploadProgressEnabled(VOD_RECORD_UPLOAD_PROGRESS_ENABLED);
+                    uploader.setPartSize(1024 * 1024);
+                    final VODUploadCallback callback = new VODUploadCallback() {
+                        @Override
+                        public void onUploadSucceed(UploadFileInfo info) {
+                            String filePath = info.getFilePath();
+                            VodUploadFileModel model = fileList.stream().filter(f -> f.isFilePath(filePath)).findFirst()
+                                    .orElse(VodUploadFileModel.empty);
+                            JSONObject jsonObject = new JSONObject();
+                            try {
+                                jsonObject.put("filePath", filePath);
+                                jsonObject.put("url", info.getVodInfo().getCoverUrl());
+                                jsonObject.put("videoId", model.getVideoId());
+                                jsonObject.put("status", info.getStatus());
+                            } catch (JSONException e) {
+                                Log.i(TAG, "onUploadSucceed: json error");
+                            }
+                            final PluginResult result = new PluginResult(PluginResult.Status.OK, jsonObject);
+                            result.setKeepCallback(true);
+                            callbackContext.sendPluginResult(result);
+                        }
+
+                        @Override
+                        public void onUploadFailed(UploadFileInfo info, String code, String message) {
+                            final String filePath = info.getFilePath();
+                            final VodUploadFileModel model = fileList.stream().filter(f -> f.isFilePath(filePath))
+                                    .findFirst().orElse(VodUploadFileModel.empty);
+                            final JSONObject jsonObject = new JSONObject();
+                            try {
+                                jsonObject.put("filePath", filePath);
+                                jsonObject.put("videoId", model.getVideoId());
+                                jsonObject.put("status", info.getStatus());
+                                jsonObject.put("code", info.getStatus());
+                                jsonObject.put("message", info.getStatus());
+                            } catch (JSONException e) {
+                                Log.i(TAG, "onUploadFailed: json error");
+                            }
+                            final PluginResult result = new PluginResult(PluginResult.Status.ERROR, jsonObject);
+                            result.setKeepCallback(true);
+                            callbackContext.sendPluginResult(result);
+                        }
+
+                        @Override
+                        public void onUploadProgress(UploadFileInfo info, long uploadedSize, long totalSize) {
+                            final String filePath = info.getFilePath();
+                            final VodUploadFileModel model = fileList.stream().filter(f -> f.isFilePath(filePath))
+                                    .findFirst().orElse(VodUploadFileModel.empty);
+                            Log.i(TAG, "onUploadProgress: " + filePath + "," + uploadedSize + "/" + totalSize);
+                            final JSONObject jsonObject = new JSONObject();
+                            try {
+                                jsonObject.put("filePath", filePath);
+                                jsonObject.put("videoId", model.getVideoId());
+                                jsonObject.put("uploadedSize", uploadedSize);
+                                jsonObject.put("totalSize", totalSize);
+                                jsonObject.put("status", info.getStatus());
+                            } catch (JSONException e) {
+                                Log.i(TAG, "onUploadProgress: json error");
+                            }
+                            final PluginResult result = new PluginResult(PluginResult.Status.OK, jsonObject);
+                            result.setKeepCallback(true);
+                            callbackContext.sendPluginResult(result);
+                        }
+
+                        @Override
+                        public void onUploadTokenExpired() {
+                            Log.i(TAG, "onUploadTokenExpired");
+                        }
+
+                        @Override
+                        public void onUploadRetry(String code, String message) {
+                            Log.i(TAG, "onUploadRetry: " + code + "," + message);
+                        }
+
+                        @Override
+                        public void onUploadRetryResume() {
+                            Log.i(TAG, "onUploadRetryResume");
+                        }
+
+                        @Override
+                        public void onUploadStarted(UploadFileInfo info) {
+                            final String filePath = info.getFilePath();
+                            Log.i(TAG, "onUploadStarted:  " + filePath);
+                            final VodUploadFileModel model = fileList.stream().filter(f -> f.isFilePath(filePath))
+                                    .findFirst().orElse(VodUploadFileModel.empty);
+                            Log.i(TAG, "onUploadStarted: auth:" + model.getUploadAuth() + " , addr: "
+                                    + model.getUploadAddress());
+                            uploader.setUploadAuthAndAddress(info, model.getUploadAuth(), model.getUploadAddress());
+                        }
+                    };
+                    Log.i(TAG, "call uploader.init");
+                    uploader.init(callback);
+                    Log.i(TAG, "call uploader.setPartSize");
+                    uploader.setPartSize(1024 * 1024);
+                    for (VodUploadFileModel model : fileList) {
+                        final VodInfo vodInfo = new VodInfo();
+                        vodInfo.setIsProcess(true);
+                        Log.i(TAG, "call uploader.addFile, filePath: " + model.getFilePath());
+                        uploader.addFile(model.getFilePath(), vodInfo);
+                    }
+                    Log.i(TAG, "call uploader.start");
+                    uploader.start();
+                }
+            });
             return true;
         }
         return false;
-    }
-
-    private void startUpload(List<VodUploadFileModel> fileList, CallbackContext callbackContext, Context context) {
-        final VODUploadClient uploader = new VODUploadClientImpl(context);
-        uploader.setRegion(VOD_REGION);
-        uploader.setRecordUploadProgressEnabled(VOD_RECORD_UPLOAD_PROGRESS_ENABLED);
-        uploader.setPartSize(1024 * 1024);
-        VODUploadCallback callback = new VODUploadCallback() {
-            @Override
-            public void onUploadSucceed(UploadFileInfo info) {
-                String filePath = info.getFilePath();
-                VodUploadFileModel model = fileList.stream().filter(f -> f.isFilePath(filePath)).findFirst()
-                        .orElse(VodUploadFileModel.empty);
-                JSONObject jsonObject = new JSONObject();
-                try {
-                    jsonObject.put("filePath", filePath);
-                    jsonObject.put("url", info.getVodInfo().getCoverUrl());
-                    jsonObject.put("videoId", model.getVideoId());
-                    jsonObject.put("status", info.getStatus());
-                } catch (JSONException e) {
-                    Log.i(TAG, "onUploadSucceed: json error");
-                }
-                PluginResult result = new PluginResult(PluginResult.Status.OK, jsonObject);
-                result.setKeepCallback(true);
-                callbackContext.sendPluginResult(result);
-            }
-
-            @Override
-            public void onUploadFailed(UploadFileInfo info, String code, String message) {
-                String filePath = info.getFilePath();
-                VodUploadFileModel model = fileList.stream().filter(f -> f.isFilePath(filePath)).findFirst()
-                        .orElse(VodUploadFileModel.empty);
-                JSONObject jsonObject = new JSONObject();
-                try {
-                    jsonObject.put("filePath", filePath);
-                    jsonObject.put("videoId", model.getVideoId());
-                    jsonObject.put("status", info.getStatus());
-                    jsonObject.put("code", info.getStatus());
-                    jsonObject.put("message", info.getStatus());
-                } catch (JSONException e) {
-                    Log.i(TAG, "onUploadFailed: json error");
-                }
-                PluginResult result = new PluginResult(PluginResult.Status.ERROR, jsonObject);
-                result.setKeepCallback(true);
-                callbackContext.sendPluginResult(result);
-            }
-
-            @Override
-            public void onUploadProgress(UploadFileInfo info, long uploadedSize, long totalSize) {
-                String filePath = info.getFilePath();
-                VodUploadFileModel model = fileList.stream().filter(f -> f.isFilePath(filePath)).findFirst()
-                        .orElse(VodUploadFileModel.empty);
-                Log.i(TAG, "onUploadProgress: " + filePath + "," + uploadedSize + "/" + totalSize);
-                JSONObject jsonObject = new JSONObject();
-                try {
-                    jsonObject.put("filePath", filePath);
-                    jsonObject.put("videoId", model.getVideoId());
-                    jsonObject.put("uploadedSize", uploadedSize);
-                    jsonObject.put("totalSize", totalSize);
-                    jsonObject.put("status", info.getStatus());
-                } catch (JSONException e) {
-                    Log.i(TAG, "onUploadProgress: json error");
-                }
-                PluginResult result = new PluginResult(PluginResult.Status.OK, jsonObject);
-                result.setKeepCallback(true);
-                callbackContext.sendPluginResult(result);
-            }
-
-            @Override
-            public void onUploadTokenExpired() {
-                Log.i(TAG, "onUploadTokenExpired");
-            }
-
-            @Override
-            public void onUploadRetry(String code, String message) {
-                Log.i(TAG, "onUploadRetry: " + code + "," + message);
-            }
-
-            @Override
-            public void onUploadRetryResume() {
-                Log.i(TAG, "onUploadRetryResume");
-            }
-
-            @Override
-            public void onUploadStarted(UploadFileInfo info) {
-                String filePath = info.getFilePath();
-                Log.i(TAG, "onUploadStarted:  " + filePath);
-                VodUploadFileModel model = fileList.stream().filter(f -> f.isFilePath(filePath)).findFirst()
-                        .orElse(VodUploadFileModel.empty);
-                Log.i(TAG, "onUploadStarted: auth:" + model.getUploadAuth() + " , addr: " + model.getUploadAddress());
-                uploader.setUploadAuthAndAddress(info, model.getUploadAuth(), model.getUploadAddress());
-            }
-        };
-        Log.i(TAG, "call uploader.init");
-        uploader.init(callback);
-        Log.i(TAG, "call uploader.setPartSize");
-        uploader.setPartSize(1024 * 1024);
-        for (VodUploadFileModel model : fileList) {
-            VodInfo vodInfo = new VodInfo();
-            vodInfo.setIsProcess(true);
-            Log.i(TAG, "call uploader.addFile, filePath: " + model.getFilePath());
-            uploader.addFile(model.getFilePath(), vodInfo);
-        }
-        Log.i(TAG, "call uploader.start");
-        uploader.start();
     }
 }
